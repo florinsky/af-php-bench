06929175d60b1508ba29f31697fcd3e3170f188b
Alexey Florinsky <af@simbirsoft.com>
2015-09-14 18:19:34 +0300

    Project updated. Several improvements

---
+++ src/benchmark/BenchmarkArraySort.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AF\Benchmark;
+
+class BenchmarkArraySort extends BenchmarkBase {
+    protected function test() {
+        $aa = microtime(true);
+        $data = $this->data;
+        $bb = microtime(true);
+
+        $limit = 1 * MULT;
+        for($i=0;$i<$limit;$i++) {
+            $b=$data; asort($b);
+            $b=$data; ksort($b);
+            $b=$data; sort($b);
+        }
+    }
+
+    function getName() {
+        return 'Array Sort';
+    }
+
+    protected $data=[
+  18849627 => 4594715,
+  79549708 => 6886075,
+  15104254 => 19364353,
+  52939704 => 6073817,
+  81053625 => 85048810,
+  17073021 => 33445470,
+  84675569 => 1148860,
+  68597489 => 10758668,
+  33849442 => 68328856,
+  5794138 => 31172474,
+];
+}
+
--- src/phpbm.php
+++ src/phpbm.php
@@ -24,6 +24,7 @@ $app->addTestGroup('STRINGS', [
 
 $app->addTestGroup('ARRAYS', [
     new AF\Benchmark\BenchmarkFillArray(['skip'=>false]),
+    new AF\Benchmark\BenchmarkArraySort(['skip'=>false]),
 ]);
 
 $app->run();
